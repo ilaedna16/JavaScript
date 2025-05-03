75cfbcf6e8817773cb045fa048380ab9a60b7b5c
Ilaedna Delmiro <115794672+ilaedna16@users.noreply.github.com>
2025-05-03 14:38:23 -0300

    Declarar um array de suas frutas favoritas.js

---
+++ Declarar um array de suas frutas favoritas.js
@@ -0,0 +1,8 @@
+// Escreva um programa que declare um array de suas frutas favoritas e 
+ //imprima cada fruta na lista usando um loop.
+ 
+ let frutas = ["Banana", "Uva", "Kiwi", "Manga"]; 
+ for (let i=0; i<frutas.length; i++) { 
+    console.log(frutas[i]); 
+ 
+ }

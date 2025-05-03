7768368ca5363a0dacb9ca086ad0c10a7abc023f
Ilaedna Delmiro <115794672+ilaedna16@users.noreply.github.com>
2025-05-03 14:50:37 -0300

    Recebendo uma string e retornando a mesma string.js

---
+++ Recebendo uma string e retornando a mesma string.js
@@ -0,0 +1,10 @@
+//Escreva uma função que receba uma string e retorne a mesma string, 
+//mas com a primeira letra de cada palavra em maiúsculas.
+
+function capitalize(str) {
+    return str.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
+
+}
+
+
+console.log(capitalize("hello world"));
